d2021507740146f088245aa3447fa21dc587bb84
Diego Johann Humpiri Vargas <dhumpiriv@ulasalle.edu.pe>
2025-09-10 20:55:19 -0500

    Fixing tables

--- sql_sripts/tablas.sql
+++ sql_sripts/tablas.sql
@@ -9,22 +9,26 @@ SELECT
     column_default,
     is_nullable
 FROM
-    information_schema.columns c
+    information_schema.columns
 WHERE
-    table_name = 'tmp';
+    table_name = 'proyecto';
 
 DROP TABLE IF EXISTS proyecto;
+DROP TABLE IF EXISTS tipo_proyecto;
+DROP TABLE IF EXISTS proyecto_objetivo_especifico;
+DROP TABLE IF EXISTS documento_proyecto;
 
 -- ------- Tabla proyecto ----------------------------------
 CREATE TABLE proyecto(
-    id                      SERIAL PRIMARY KEY,
+    id                      INTEGER PRIMARY KEY,
     nombre                  VARCHAR(200),
     email_lider             VARCHAR(200),
-    id_carpeta             VARCHAR(200),
+    id_carpeta              VARCHAR(200),
     objetivo_general        VARCHAR(500),
     fecha_creacion          TIMESTAMPTZ,
     fecha_inicio            TIMESTAMPTZ,
     fecha_fin               TIMESTAMPTZ,
+    estado                  BOOLEAN DEFAULT TRUE,
 
     id_tipo_proyecto        INTEGER,
     id_hoja_seguimiento     INTEGER,
@@ -32,67 +36,157 @@ CREATE TABLE proyecto(
     id_estado_proyecto      INTEGER,
 
     created_by              INTEGER,
-    created_date            TIMESTAMPTZ,
+    created_date            TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     modified_by             INTEGER,
     modified_date           TIMESTAMPTZ
 );
+
+CREATE SEQUENCE secuencia_proyecto
+    START WITH 101
+    INCREMENT BY 1
+    MINVALUE 101
+    MAXVALUE 999999;
+
+ALTER TABLE 
+    proyecto 
+ALTER 
+    COLUMN id 
+    SET DEFAULT nextval('secuencia_proyecto');
 
 -- ------- Tabla tipo_proyecto -----------------------------
 CREATE TABLE tipo_proyecto(
-    id                      SERIAL PRIMARY KEY,
+    id                      INTEGER PRIMARY KEY,
     nombre                  VARCHAR(200),
+    estado                  BOOLEAN DEFAULT TRUE,
 
     created_by              INTEGER,
-    created_date            TIMESTAMPTZ,
+    created_date            TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     modified_by             INTEGER,
     modified_date           TIMESTAMPTZ
 );
+
+CREATE SEQUENCE secuencia_tipo_proyecto
+    START WITH 101
+    INCREMENT BY 1
+    MINVALUE 101
+    MAXVALUE 999999;
+
+ALTER TABLE 
+    tipo_proyecto 
+ALTER 
+    COLUMN id 
+    SET DEFAULT nextval('secuencia_tipo_proyecto');
 
 -- ------- Tabla proyecto_objetivo_especifico --------------
 CREATE TABLE proyecto_objetivo_especifico(
-    id                      SERIAL PRIMARY KEY,
+    id                      INTEGER PRIMARY KEY,
     objetivo_especifico     VARCHAR(500),
+    estado                  BOOLEAN DEFAULT TRUE,
 
     id_proyecto             INTEGER,
 
     created_by              INTEGER,
-    created_date            TIMESTAMPTZ,
+    created_date            TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     modified_by             INTEGER,
     modified_date           TIMESTAMPTZ
 );
+
+CREATE SEQUENCE secuencia_proyecto_objetivo_especifico
+    START WITH 101
+    INCREMENT BY 1
+    MINVALUE 101
+    MAXVALUE 999999;
+
+ALTER TABLE 
+    proyecto_objetivo_especifico
+ALTER 
+    COLUMN id 
+    SET DEFAULT nextval('secuencia_proyecto_objetivo_especifico');
 
 -- ------- Tabla documento_proyecto ------------------------
 CREATE TABLE documento_proyecto(
-    id_documento            SERIAL PRIMARY KEY,
+    id                      INTEGER PRIMARY KEY,
     titulo                  VARCHAR(200),
     tiene_marcadores        BOOLEAN,
+    estado                  BOOLEAN DEFAULT TRUE,
 
     id_proyecto             INTEGER,
 
     created_by              INTEGER,
-    created_date            TIMESTAMPTZ,
+    created_date            TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     modified_by             INTEGER,
     modified_date           TIMESTAMPTZ
 );
 
-insert into tipo_proyecto (nombre, created_by, created_date, modified_by, modified_date )
+CREATE SEQUENCE secuencia_documento_proyecto
+    START WITH 101
+    INCREMENT BY 1
+    MINVALUE 101
+    MAXVALUE 999999;
+
+ALTER TABLE 
+    documento_proyecto
+ALTER 
+    COLUMN id 
+    SET DEFAULT nextval('secuencia_documento_proyecto');
+
+-- ---------- Funciones para los triggers ----------------
+
+CREATE OR REPLACE FUNCTION actualizar_modified_date()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.modified_date = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- ----------- Triggers ----------------------------------
+
+CREATE TRIGGER trigger_actualizar_proyecto
+BEFORE UPDATE ON proyecto
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_modified_date();
+
+CREATE TRIGGER trigger_actualizar_tipo_proyecto
+BEFORE UPDATE ON tipo_proyecto
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_modified_date();
+
+CREATE TRIGGER trigger_actualizar_proyecto_objetivo_especifico
+BEFORE UPDATE ON proyecto_objetivo_especifico
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_modified_date();
+
+CREATE TRIGGER trigger_actualizar_documento_proyecto
+BEFORE UPDATE ON documento_proyecto
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_modified_date();
+
+-- ---------- Mock Data-----------------------------------
+
+insert into tipo_proyecto 
+(nombre, created_by)
 values (
-        'Proyecto Interno', 1, now(), null, null
+        'Proyecto Interno', 1
 );
 
 insert into proyecto
-( nombre, email_lider, id_carpeta, objetivo_general, fecha_creacion, fecha_inicio, fecha_fin, id_tipo_proyecto, id_hoja_seguimiento, id_grupo_asignado, id_estado_proyecto, created_by, created_date, modified_by, modified_date)
+(nombre, email_lider, id_carpeta, objetivo_general, fecha_creacion, fecha_inicio, fecha_fin, id_tipo_proyecto, id_hoja_seguimiento, id_grupo_asignado, id_estado_proyecto, created_by)
 values (
-        'Proyecto 1','lider@mail.com', 'asdf', 'objetivo general 1', now(), null, null, 1, null, null, null, 1, now(), null, null
-       );
+    'Proyecto 1','lider@mail.com', 'asdf', 'objetivo general 1', null, null, 1, null, null, null, 1
+);
 
-insert into documento_proyecto (titulo, tiene_marcadores, id_proyecto, created_by, created_date, modified_by, modified_date)
+insert into documento_proyecto 
+(titulo, tiene_marcadores, id_proyecto, created_by)
 values (
-        'documento 1', false, 1, 1, now(), null, null
-       );
+    'documento 1', false, 1, 1
+);
 
-insert into proyecto_objetivo_especifico (objetivo_especifico, id_proyecto, created_by, created_date, modified_by, modified_date)
-values ('Objetivo específico 1', 1, 1, now(), null, null);
+insert into proyecto_objetivo_especifico 
+(objetivo_especifico, id_proyecto, created_by)
+values (
+    'Objetivo específico 1', 1, 1
+);
 
 select * from tipo_proyecto;
 select * from proyecto;
