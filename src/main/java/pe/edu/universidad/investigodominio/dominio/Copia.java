51c81fbd913408dc4d9019efae36b340f5ad4cae
David5G <josshuadavid5g@gmail.com>
2025-09-12 14:22:54 -0500

    fix(Copia): Mejorando la logica de la Entidad Copia

--- src/main/java/pe/edu/universidad/investigodominio/dominio/Copia.java
+++ src/main/java/pe/edu/universidad/investigodominio/dominio/Copia.java
@@ -1,94 +1,36 @@
 package pe.edu.universidad.investigodominio.dominio;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.OffsetDateTime;
 
+@Data
+@Table(name = "copia")
 @Entity
 public class Copia {
 
     @Id
-    private int idCopia;
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "nombre")
     private String nombre;
-    private String responsablesCopias;
-    private OffsetDateTime fechaCreacion;
-    private int idReparto;
+    @Column(name = "responsables")
+    private String responsables;
+    @Column(name = "estado")
+    private boolean estado;
+
+    @Column(name = "created_by")
     private int createdBy;
+    @Column(name = "created_date")
     private OffsetDateTime createdDate;
+    @Column(name = "modified_by")
     private int modifiedBy;
+    @Column(name = "modified_date")
     private OffsetDateTime modifiedDate;
 
-    public int getIdCopia() {
-        return idCopia;
-    }
-
-    public void setIdCopia(int idCopia) {
-        this.idCopia = idCopia;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getResponsablesCopias() {
-        return responsablesCopias;
-    }
-
-    public void setResponsablesCopias(String responsablesCopias) {
-        this.responsablesCopias = responsablesCopias;
-    }
-
-    public OffsetDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(OffsetDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public int getIdReparto() {
-        return idReparto;
-    }
-
-    public void setIdReparto(int idReparto) {
-        this.idReparto = idReparto;
-    }
-
-    public int getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(int createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public OffsetDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(OffsetDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public int getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(int modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public OffsetDateTime getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(OffsetDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
 }
