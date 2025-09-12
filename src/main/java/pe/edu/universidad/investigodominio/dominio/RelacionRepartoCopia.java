fe7e750e3c424081e55eb0003cb5fcb20dd2b02f
David5G <josshuadavid5g@gmail.com>
2025-09-12 14:57:31 -0500

    fix(RelacionRepartoCopia): Mejorando la logica de la Entidad RelacionRepartoCopia

--- src/main/java/pe/edu/universidad/investigodominio/dominio/RelacionRepartoCopia.java
+++ src/main/java/pe/edu/universidad/investigodominio/dominio/RelacionRepartoCopia.java
@@ -1,94 +1,34 @@
 package pe.edu.universidad.investigodominio.dominio;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.OffsetDateTime;
 
+@Data
+@Table(name = "relacion_reparto_copia")
 @Entity
 public class RelacionRepartoCopia {
 
     @Id
-    private int idRelacionRepartoCopia;
+    @Column(name = "id")
+    private int id;
 
-    private int idReparto;
-    private int idCopia;
+    @Column(name = "email")
     private String email;
-    private String estado;
+    @Column(name = "estado")
+    private boolean estado;
+
+    @Column(name = "created_by")
     private int createdBy;
+    @Column(name = "created_date")
     private OffsetDateTime createdDate;
+    @Column(name = "modified_by")
     private int modifiedBy;
+    @Column(name = "modified_date")
     private OffsetDateTime modifiedDate;
 
-    public int getIdRelacionRepartoCopia() {
-        return idRelacionRepartoCopia;
-    }
-
-    public void setIdRelacionRepartoCopia(int idRelacionRepartoCopia) {
-        this.idRelacionRepartoCopia = idRelacionRepartoCopia;
-    }
-
-    public int getIdReparto() {
-        return idReparto;
-    }
-
-    public void setIdReparto(int idReparto) {
-        this.idReparto = idReparto;
-    }
-
-    public int getIdCopia() {
-        return idCopia;
-    }
-
-    public void setIdCopia(int idCopia) {
-        this.idCopia = idCopia;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(int createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public OffsetDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(OffsetDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public int getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(int modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public OffsetDateTime getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(OffsetDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
 }
