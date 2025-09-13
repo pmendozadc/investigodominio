da0b93a5c3c040de0db9851d4d946df18244b09b
David5G <josshuadavid5g@gmail.com>
2025-09-12 22:20:14 -0500

    docs(triggers.sql): Agregar triggers usados en la base de datos postgresql

---
+++ sql/triggers.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION establecer_modified_date()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_date = (CURRENT_TIMESTAMP AT TIME ZONE 'America/Lima')::timestamptz;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_copia_update
+BEFORE UPDATE ON copia
+FOR EACH ROW
+EXECUTE FUNCTION establecer_modified_date();
+
+CREATE OR REPLACE TRIGGER trg_historial_copia_update
+BEFORE UPDATE ON historial_copia
+FOR EACH ROW
+EXECUTE FUNCTION establecer_modified_date();
+
+CREATE OR REPLACE TRIGGER trg_relacion_reparto_copia_update
+BEFORE UPDATE ON relacion_reparto_copia
+FOR EACH ROW
+EXECUTE FUNCTION establecer_modified_date();
