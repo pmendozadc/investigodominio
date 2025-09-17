2f41d95daede7ef890e023972c35933c77754417
c16219 <c16219@UTP398092.grupoutp.net>
2025-09-17 15:02:05 -0500

    Agregado de test de get y post

--- src/main/java/pe/edu/universidad/investigodominio/controller/ControllerData.java
+++ src/main/java/pe/edu/universidad/investigodominio/controller/ControllerData.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import pe.edu.universidad.investigodominio.dominio.Proyecto;
 import pe.edu.universidad.investigodominio.service.ServiceGenerico;
 
 @RestController
--- src/test/java/pe/edu/universidad/investigodominio/TestControllerData.java
+++ src/test/java/pe/edu/universidad/investigodominio/TestControllerData.java
@@ -2,9 +2,13 @@ package pe.edu.universidad.investigodominio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,14 +16,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestControllerData {
 
 	@Autowired
     private TestRestTemplate restTemplate;
 
+	@Test
+	@Order(1)
+    void testPostProyecto() {
+		Map<String, String> mapProyecto = new HashMap<>();
+		mapProyecto.put("nombre", "Proyecto de ejemplo");
+		mapProyecto.put("emailLider", "lider@gmail.com");
+        ResponseEntity<Map> response = restTemplate.postForEntity("/c/Proyecto", mapProyecto, Map.class);
+        System.out.println(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        //Map<String, Object> map = response.getBody();
+        
+    }
+	
     @Test
-    void testGetUsuario() {
-        ResponseEntity<Map> response = restTemplate.getForEntity("/usuarios/1", Map.class);
+    @Order(2)
+    void testGetProyecto() {
+        ResponseEntity<Map> response = restTemplate.getForEntity("/r/Proyecto/1001", Map.class);
+        System.out.println(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         //Map<String, Object> map = response.getBody();
         
