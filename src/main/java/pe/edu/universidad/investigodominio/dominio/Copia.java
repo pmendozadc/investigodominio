206169f3762841c8b84aeccdd85b88c123f4bb2c
David5G <josshuadavid5g@gmail.com>
2025-09-18 16:57:00 -0500

    refactor(Copia): Reemplaza Lombok por getters y setters manuales

--- src/main/java/pe/edu/universidad/investigodominio/dominio/Copia.java
+++ src/main/java/pe/edu/universidad/investigodominio/dominio/Copia.java
@@ -4,13 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
 
 import java.time.OffsetDateTime;
 
-@Data
 @Table(name = "copia")
 @Entity
+@DynamicInsert
 public class Copia {
 
     @Id
@@ -22,15 +22,65 @@ public class Copia {
     @Column(name = "responsables")
     private String responsables;
     @Column(name = "estado")
-    private Boolean estado = true;
+    private Boolean estado;
 
     @Column(name = "created_by")
     private Integer createdBy;
-    @Column(name = "created_date",insertable = false, updatable = false)
+    @Column(name = "created_date")
     private OffsetDateTime createdDate;
     @Column(name = "modified_by")
     private Integer modifiedBy;
-    @Column(name = "modified_date",insertable = false, updatable = false)
+    @Column(name = "modified_date")
     private OffsetDateTime modifiedDate;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getResponsables() {
+        return responsables;
+    }
+    public void setResponsables(String responsables) {
+        this.responsables = responsables;
+    }
+    public Boolean getEstado() {
+        return estado;
+    }
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+    public OffsetDateTime getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(OffsetDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+    public Integer getModifiedBy() {
+        return modifiedBy;
+    }
+    public void setModifiedBy(Integer modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+    public OffsetDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+    public void setModifiedDate(OffsetDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 
 }
