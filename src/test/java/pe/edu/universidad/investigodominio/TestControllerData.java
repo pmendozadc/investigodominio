7678994c0c603649e5e597e893c79a777dcd620b
Paul Mendoza del Carpio <p.mendozadc@ulasalle.edu.pe>
2025-09-18 17:04:52 -0500

    Se actualiza con merge. Se agregaron pruebas para put y deletes

--- src/test/java/pe/edu/universidad/investigodominio/TestControllerData.java
+++ src/test/java/pe/edu/universidad/investigodominio/TestControllerData.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -22,7 +24,7 @@ public class TestControllerData {
 	@Autowired
     private TestRestTemplate restTemplate;
 
-	@Test
+	//@Test
 	@Order(1)
     void testPostProyecto() {
 		Map<String, String> mapProyecto = new HashMap<>();
@@ -31,8 +33,6 @@ public class TestControllerData {
         ResponseEntity<Map> response = restTemplate.postForEntity("/c/Proyecto", mapProyecto, Map.class);
         System.out.println("POST Response: "+response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        //Map<String, Object> map = response.getBody();
-        
     }
 	
     @Test
@@ -41,7 +41,50 @@ public class TestControllerData {
         ResponseEntity<Map> response = restTemplate.getForEntity("/r/Proyecto/1001", Map.class);
         System.out.println("GET Response: "+response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        //Map<String, Object> map = response.getBody();
-        
+    }
+    
+    @Test
+	@Order(3)
+    void testPutProyecto() {
+		Map<String, Object> mapProyecto = new HashMap<>();
+		mapProyecto.put("id", 1002);
+		mapProyecto.put("nombre", "Proyecto de ejemplo de prueba actualizado");
+		mapProyecto.put("emailLider", "lider@gmail.com");
+        ResponseEntity<Map> response = restTemplate.exchange(
+        		"/u/Proyecto",
+                HttpMethod.PUT,
+                new HttpEntity<>(mapProyecto), 
+                Map.class
+            );
+        System.out.println("PUT Response: "+response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+	@Order(4)
+    void testDeleteProyecto() {
+		int id = 1006;
+        ResponseEntity<Map> response = restTemplate.exchange(
+        		"/d/Proyecto/"+id,
+                HttpMethod.DELETE,
+                null, 
+                Map.class
+            );
+        System.out.println("DELETE Response: "+response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+	@Order(4)
+    void testDeleteHardProyecto() {
+		int id = 1013;
+        ResponseEntity<Map> response = restTemplate.exchange(
+        		"/dh/Proyecto/"+id,
+                HttpMethod.DELETE,
+                null, 
+                Map.class
+            );
+        System.out.println("DELETE HARD Response: "+response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
