eec1f1ff565684b08feb68760968809fc6d220ba
c16219 <c16219@UTP398111.grupoutp.net>
2025-10-24 17:17:44 -0500

    Actualizacion para documentacion de los endpoints con OpenAPI

--- src/main/java/pe/edu/universidad/investigodominio/controller/ControllerData.java
+++ src/main/java/pe/edu/universidad/investigodominio/controller/ControllerData.java
@@ -15,9 +15,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import pe.edu.universidad.investigodominio.dominio.Proyecto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import pe.edu.universidad.investigodominio.service.ServiceGenerico;
 
+/**
+ * Incluye documentacion OpenAPI para su generacion en la ruta /swagger-ui/index.html
+ */
 @RestController
 public class ControllerData {
 
@@ -26,20 +33,95 @@ public class ControllerData {
 	@Autowired
 	private ServiceGenerico serviceGenerico;
 	
+	@Operation(
+	    summary = "Ejecutar una transaccion, la cual contiene multiples operaciones",
+	    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+	        content = @Content(
+	            mediaType = "application/json",
+	            schema = @Schema(type = "object", description = "Lista de objetos Operacion (c/r/u/d/dh/i/a)"),
+	            examples = { 
+            	@ExampleObject(
+	                name = "Ejemplo con insert(c) y update(u)",
+	                value = """
+                    [
+                    {"op":"c","entidad":"Proyecto","obj":{"nombre":"Proyecto tx 11"},"ret":true},
+                	{"op":"u","entidad":"Proyecto","obj":{"id":1001,"nombre":"Proyecto tx actualizado","datIdHojaSeguimiento":"askldklasjd87cxzc8as8das"},"ret":true}
+                	]
+                    """
+	            ),
+	            @ExampleObject(
+	                name = "Ejemplo con insert(c) y update(u) que usa el id generado",
+	                value = """
+                    [
+                    {"op":"c","entidad":"Proyecto","obj":{"nombre":"Proyecto tx 11"},"ret":true,"key":"idProyecto"},
+                	{"op":"u","entidad":"Proyecto","obj":{"id":1001,"nombre":"Proyecto tx actualizado","datIdHojaSeguimiento":"#keyidProyecto"},"ret":true}
+                	]
+                    """
+	            ),
+	            @ExampleObject(
+	                name = "Ejemplo con select(q), e insert(c)",
+	                value = """
+                    [{"op":"q","sql":"SELECT id, nombre FROM proyecto","ret":true},
+                    {"op":"c","entidad":"Proyecto","obj":{"nombre":"Proyecto tx 11"},"ret":true}
+                	]
+                    """
+	            ),
+	            @ExampleObject(
+	                name = "Ejemplo con select(q) que retorna una sola columna, e insert(c) que se ejecuta iterativamente de acuerdo a los resultados del select",
+	                value = """
+	                [{"op":"q","sql":"SELECT nombre FROM proyecto ORDER BY nombre","ret":true,"var":"nombreProyecto"},
+	                {"op":"c","entidad":"Proyecto","for":"nombreProyecto", "obj":{"nombre":"Proyecto tx","datIdHojaSeguimiento":"#var"},"ret":true}
+	                ]
+                    """
+	            ),
+	            @ExampleObject(
+	                name = "Ejemplo con select(q) que retorna mas de una columna, e insert(c) que se ejecuta iterativamente de acuerdo a los resultados del select",
+	                value = """
+	                [{"op":"q","sql":"SELECT id,nombre FROM proyecto ORDER BY nombre","ret":true,"var":"nombreProyecto"},
+	                {"op":"c","entidad":"Proyecto","for":"nombreProyecto", "obj":{"nombre":"Proyecto tx","datIdHojaSeguimiento":"#var[1]"},"ret":true}
+	                ]
+                    """
+	            )
+	            }
+	        )
+	    )
+	)
 	@PostMapping("/tx")
 	public ResponseEntity postTransaccion(@RequestBody List<Map<String, Object>> lst) {
 	    List<Object> lstRetorno = serviceGenerico.ejecutarTx(lst);
 	    return ResponseEntity.ok(lstRetorno);
 	}
 	
+	@Operation(
+	    summary = "Insertar un objeto entidad en la tabla correspondiente",
+	    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+	        content = @Content(
+	            mediaType = "application/json",
+	            schema = @Schema(type = "object", description = "Objeto que contiene datos de acuerdo a la tabla de la entidad"),
+	            examples = @ExampleObject(
+	                name = "Ejemplo para entidad Proyecto que tiene la columna nombre",
+	                value = """
+                    {"nombre":"Proyecto tx 11"}
+                    """
+	            )
+	        )
+	    )
+	)
 	@PostMapping("/c/{entidad}")
-	public ResponseEntity postEntidad(@PathVariable("entidad") String entidadNombre, @RequestBody Map<String, Object> map) {
+	public ResponseEntity postEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, @RequestBody Map<String, Object> map) {
 	    Object obj = serviceGenerico.create(entidadNombre, map);
 	    return ResponseEntity.ok(obj);
 	}
 	
+	@Operation(summary = "Consultar un objeto entidad por su id en la tabla correspondiente")
 	@GetMapping("/r/{entidad}/{id}")
-	public ResponseEntity getEntidad(@PathVariable("entidad") String entidadNombre, @PathVariable("id") int id) {
+	public ResponseEntity getEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, 
+			@Parameter(description = "Id del objeto a consultar",example = "1001",required = true,schema = @Schema(type = "int"))
+			@PathVariable("id") int id) {
 	    Optional entidadOpt = serviceGenerico.read(entidadNombre, id);
 	    if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -49,8 +131,25 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(
+	    summary = "Actualizar un objeto entidad en la tabla correspondiente",
+	    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+	        content = @Content(
+	            mediaType = "application/json",
+	            schema = @Schema(type = "object", description = "Objeto que contiene el campo id y los datos de acuerdo a la tabla de la entidad"),
+	            examples = @ExampleObject(
+	                name = "Ejemplo para entidad Proyecto que tiene la columna nombre",
+	                value = """
+                    {"id":1001,"nombre":"Proyecto actualizado"}
+                    """
+	            )
+	        )
+	    )
+	)
 	@PutMapping("/u/{entidad}")
-	public ResponseEntity putEntidad(@PathVariable("entidad") String entidadNombre, @RequestBody Map<String, Object> map) {
+	public ResponseEntity putEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, @RequestBody Map<String, Object> map) {
 		Optional entidadOpt = serviceGenerico.update(entidadNombre, map);
 		if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -60,8 +159,13 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(summary = "Eliminar logicamente un objeto entidad por id")
 	@DeleteMapping("/d/{entidad}/{id}")
-	public ResponseEntity deleteEntidad(@PathVariable("entidad") String entidadNombre, @PathVariable("id") int id) {
+	public ResponseEntity deleteEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, 
+			@Parameter(description = "Id del objeto a eliminar",example = "1001",required = true,schema = @Schema(type = "int"))
+			@PathVariable("id") int id) {
 		Optional entidadOpt = serviceGenerico.delete(entidadNombre, id);
 		if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -71,8 +175,13 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(summary = "Eliminar fisicamente un objeto entidad por id")
 	@DeleteMapping("/dh/{entidad}/{id}")
-	public ResponseEntity deleteHardEntidad(@PathVariable("entidad") String entidadNombre, @PathVariable("id") int id) {
+	public ResponseEntity deleteHardEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, 
+			@Parameter(description = "Id del objeto a eliminar",example = "1001",required = true,schema = @Schema(type = "int"))
+			@PathVariable("id") int id) {
 		Optional entidadOpt = serviceGenerico.deleteHard(entidadNombre, id);
 		if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -82,8 +191,13 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(summary = "Activar un objeto entidad por id. Actualiza la columna activo de la tabla correspondiente.")
 	@PutMapping("/a/{entidad}/{id}")
-	public ResponseEntity activarEntidad(@PathVariable("entidad") String entidadNombre, @PathVariable("id") int id) {
+	public ResponseEntity activarEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, 
+			@Parameter(description = "Id del objeto a activar",example = "1001",required = true,schema = @Schema(type = "int"))
+			@PathVariable("id") int id) {
 		Optional entidadOpt = serviceGenerico.activar(entidadNombre, id);
 		if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -93,8 +207,13 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(summary = "Inactivar un objeto entidad por id. Actualiza la columna activo de la tabla correspondiente.")
 	@PutMapping("/i/{entidad}/{id}")
-	public ResponseEntity inactivarEntidad(@PathVariable("entidad") String entidadNombre, @PathVariable("id") int id) {
+	public ResponseEntity inactivarEntidad(
+			@Parameter(description = "Nombre de la tabla en estilo CamelCase",example = "CuentaGrupo",required = true,schema = @Schema(type = "String"))
+			@PathVariable("entidad") String entidadNombre, 
+			@Parameter(description = "Id del objeto a inactivar",example = "1001",required = true,schema = @Schema(type = "int"))
+			@PathVariable("id") int id) {
 		Optional entidadOpt = serviceGenerico.inactivar(entidadNombre, id);
 		if (entidadOpt.isEmpty()) {
 	        Map<String, String> response = new HashMap<>();
@@ -104,6 +223,28 @@ public class ControllerData {
 	    return ResponseEntity.ok(entidadOpt.get());
 	}
 	
+	@Operation(
+	    summary = "Ejecutar una consulta SQL",
+	    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+	        content = @Content(
+	            mediaType = "application/json",
+	            schema = @Schema(type = "object", description = "Objeto que contiene el campo sql, el cual contiene el SELECT a ejecutar."
+	            		+ " El campo sql tambien puede ser un arreglo de cadenas con los SELECT."),
+	            examples = {@ExampleObject(
+	                name = "Ejemplo con un solo SELECT",
+	                value = """
+                    {"sql":"SELECT id, nombre FROM proyecto"}
+                    """
+	            ),
+        		@ExampleObject(
+	                name = "Ejemplo con multiples SELECT",
+	                value = """
+                    {"sql":["SELECT id, nombre FROM proyecto","SELECT MAX(nombre) FROM proyecto"]}
+                    """
+	            )}
+	        )
+	    )
+	)
 	@PostMapping("/q")
 	public ResponseEntity consultarSQLNativo(@RequestBody Map<String, Object> map) {
 		Object obj = map.get(strSql);
